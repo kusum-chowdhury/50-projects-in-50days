a01d5b4493eb9f753b7bd65a500dfffda28439a0
kusum chowdhury <kusumchowdhury5832@gmail.com>
2023-01-13 03:55:53 +0530

    dad jokes

---
+++ dad-jokes/script.js
@@ -0,0 +1,21 @@
+const joke = document.querySelector('.joke')
+const jokeBtn = document.querySelector('.jokeBtn')
+
+jokeBtn.addEventListener('click', generateJoke)
+
+generateJoke()
+
+// USING ASYNC/AWAIT
+async function generateJoke() {
+  const config = {
+    headers: {
+      Accept: 'application/json',
+    },
+  }
+
+  const res = await fetch('https://icanhazdadjoke.com', config)
+
+  const data = await res.json()
+
+  joke.innerHTML = data.joke;
+}
